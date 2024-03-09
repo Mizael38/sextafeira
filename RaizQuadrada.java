315133effe2368e9894ab7fc4af001b97d3bde80
Mizael <miz-black1@hotmail.com>
2024-03-08 21:26:12 -0300

    segundo commit

---
+++ RaizQuadrada.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class RaizQuadrada {
+    public static void main(String[] args) { 
+    Scanner sc = new Scanner (System.in);
+    System.out.println("digite um numero ");
+    double numero = sc.nextdouble();
+    
+}
